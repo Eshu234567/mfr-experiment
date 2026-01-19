d52218d6e67341feb2f11065e5fd96fbc3f2fbe0
Eshu234567 <sahueshita0@gmail.com>
2026-01-19 13:38:52 +0530

    Server react frontend for render deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
+const path = require('path');
+
 
 const app = express();
 const PORT = 5000;
@@ -283,8 +285,15 @@ app.delete('/api/runs/:usn', (req, res) => {
     }
   );
 });
+// Serve React build
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 // ---------- SERVER ----------
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
